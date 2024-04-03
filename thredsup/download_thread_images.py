197a8a2fba428fdcdb259061a86619ab363e0b74
Dyno <dghule@inika.app>
2024-04-04 00:06:27 +0530

    thred up data downloading lgic added

---
+++ thredsup/download_thread_images.py
@@ -0,0 +1,74 @@
+import os
+import json
+import urllib.request
+import uuid
+import csv
+from urllib.parse import urlparse, urlunparse, parse_qs
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
+
+
+def download_image(image_url, output_dir):
+    image_name = str(uuid.uuid4()) + '.jpg'  # Generate a UUID for the image filename
+    output_path = os.path.join(output_dir, image_name)
+
+    # Create a request object with a custom User-Agent header
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
+    req = urllib.request.Request(image_url, headers=headers)
+
+    # Open the URL and retrieve the data
+    with urllib.request.urlopen(req) as response:
+        with open(output_path, 'wb') as out_file:
+            out_file.write(response.read())
+
+    print(f"Downloaded {image_name}")
+    return image_name
+
+
+def clean_image_url(image_url):
+    parsed_url = urlparse(image_url)
+    query_params = parse_qs(parsed_url.query)
+    if 'w' in query_params:
+        del query_params['w']
+    cleaned_url = parsed_url._replace(query='')
+    return urlunparse(cleaned_url)
+
+def main():
+    # Path to the JSON file
+    json_file = 'all_threds_up_data_renamed.json'
+
+    # Directory to store the images
+    output_dir = 'thredup_images'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Open CSV file for writing
+    csv_file = 'thredup_product_data.csv'
+    with open(csv_file, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['product_title', 'product_details', 'product_color', 'product_url', 'image_url', 'image_path']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        with open(json_file, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            for item in data:
+                try:
+                    image_url = item.get('image_url')
+                    if image_url:
+                        cleaned_url = clean_image_url(image_url)
+                        try:
+                            image_name = download_image(cleaned_url, output_dir)
+                            item['image_path'] = os.path.join(output_dir, image_name)
+                            item['image_url'] = cleaned_url
+                        except:
+                            item['image_path'] = "Not downloaded"
+                            item['image_url'] = cleaned_url
+                    writer.writerow(item)
+                except:
+                    print(item)
+
+    print(f"CSV file '{csv_file}' created successfully.")
+
+if __name__ == "__main__":
+    main()
--- thredsup/thredup.py
+++ thredsup/thredup.py
@@ -7,11 +7,18 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from concurrent.futures import ThreadPoolExecutor
 
 all_threds_products_data = []
+chrome_options = webdriver.ChromeOptions()
 
-
-
+# Add options
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
+chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration, can help with headless mode
+chrome_options.add_argument("--window-size=1920x1080")  # Set window size
+chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+chrome_options.add_argument("user-agent= Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320")
 
 def make_click_at_right_corner(driver):
     # Calculate the coordinates of the bottom-right corner
@@ -25,7 +32,6 @@ def make_click_at_right_corner(driver):
     actions.move_by_offset(bottom_right_x, bottom_right_y)
     actions.click()
     actions.perform()
-count = 0
 
 def get_products_list(driver, url):
     driver.get(url)
@@ -39,74 +45,59 @@ def get_products_list(driver, url):
 
     return product_list
 
-
-def extract_product_description(driver, url):
-    # driver = webdriver.Chrome(options=chrome_options)
-    global count
-    count += 1
-    print(count)
-
+def extract_product_description( url):
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     time.sleep(2)
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
 
-    # Find the <ul> tag with class "list-disc black"
     ul_tag = soup.find_all('ul', class_='list-disc black')
     product_description = []
 
-    # Extract the content within the <ul> tag
     if ul_tag:
         for tag in ul_tag:
             list_items = tag.find_all('li', class_="u-mr-1x")
             for item in list_items:
                 product_description.append(item.text.strip())
     driver.close()
-    # driver.quit()
     return ", ".join(product_description)
 
-
-def extract_product_info(driver, product):
+count=0
+def extract_product_info(product):
+    global count
     img = product.find("img", class_="BQ6FcxrnnnmSna3sncgu")
-
-    # Extracting type
     type_tag = product.find('div', class_='spot-grey-5')
     type_info = type_tag.text.strip() if type_tag else None
 
-    # Extracting link
     link_tag = product.find('a', class_='wAhTkKjWOmWyIy2F13MZ')
     link = link_tag['href'] if link_tag else ""
     base = "https://www.thredup.com"
 
-    description = extract_product_description(driver, base + link) if link != "" else ""
-
+    description = extract_product_description( base + link) if link != "" else ""
+    count += 1
+    print(count)
     result = {"Description:": description,
               "Type:": type_info,
               "Link:": base + link,
               "Image link": img["src"] if img else ""}
-    try:
-        driver.close()
-    except:
-        pass
-
-
+    all_threds_products_data.append(result)
+    insert_update_all_threadsup_data()
     return result
 
+def get_products_data_from_thredup(numOfPages=1, fromPage=1):
 
-def get_products_data_from_thredup(numOfPages=1):
-    driver = webdriver.Chrome()
-    for n in range(numOfPages):
-        url = f'https://www.thredup.com/women?department_tags=women&page={2}'
-        product_list_html = get_products_list(driver, url)
-
-        for product in product_list_html:
-            product_info = extract_product_info(driver, product)
-            all_threds_products_data.append(product_info)
-            insert_update_all_threadsup_data()
+    for n in range(fromPage, numOfPages):
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            driver = webdriver.Chrome()
+            url = f'https://www.thredup.com/women?department_tags=women&page={n}'
+            product_list_html = get_products_list(driver, url)
+            print("current page ", n)
+            for product in product_list_html:
+                executor.submit(extract_product_info, product)
 
 def insert_update_all_threadsup_data():
-    with open("all_threds_up_data_headless.json", "w") as f:
+    with open("all_threds_up_data_headless_1.json", "w") as f:
         json.dump(all_threds_products_data, f)
 
-
-get_products_data_from_thredup(1)
+get_products_data_from_thredup(2, 1)
